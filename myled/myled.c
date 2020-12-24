72fc9d9843ddcd51fcad39eb44a4b0603d58c8ab
Yyoshinuma <72370836+Yyoshinuma@users.noreply.github.com>
2020-12-24 21:13:02 +0900

    Update myled.c

--- myled/myled.c
+++ myled/myled.c
@@ -28,7 +28,6 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 	if(copy_from_user(&c,buf,sizeof(char)))
 		return -EFAULT;
 
-//	printk(KERN_INFO "receive %c\n", c);
 	if(c == '0')
 		gpio_base[10] = 1 << 25;
 	else if(c == '1'){
@@ -82,10 +81,10 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 					gpio_base[10]=1<<25;
 					msleep (200);
 					break;
-					}
 				}
 			}
-	return 1;
+		}
+return 1;
 }
 
 static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos){
